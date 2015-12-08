e42a00897f7f16ddfd2d3629a3975b68f3c2b4c1
Kristen Haydel <kristen@whatever.com>
2015-12-08 15:52:12 -0600

    Implemented the DropDown

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,39 +1,27 @@
 // YOUR CODE HERE:
 var app = {
-  allMessages: [],
+  messageStorage: {},
+  roomStorage: {},
   displayAllMessages: function() {
-    for (var i = 0; i < this.allMessages.length; i++) {
-      if (this.allMessages[i].displayTest === false) {
+    for (var key in this.messageStorage) {
         var element = $(document.createElement('div'));
-        element.text(_.escape(this.allMessages[i].username) + ': ' + _.escape(this.allMessages[i].text));
+        element.text(_.escape(this.messageStorage[key].username) + ': ' + _.escape(this.messageStorage[key].text));
         $('.all-messages').append(element);
-        this.allMessages[i].displayTest = true;
       }
-    }
   },
-  fetch: function() {
+  fetch: function(typeOfRequest, queries) {
     $.ajax({
       url: this.server,
-      type: 'GET',
-      data: 'order=-createdAt',
+      type: typeOfRequest,
+      data: queries,
       success: function(data) {
-        console.log('chatterbox: Message sent. Data: ', data);
-        // loop through data and compare data on server to already existing array above
-        // if allMessage.length = 0
-        if (app.allMessages.length === 0) {
-          app.allMessages = data.results;
-        } else {
-          for (i = 0; i < data.results.length; i++) {
-            // also loop through allMesssage and check data.results[i].objectId === allMessages[i].objectId
-            for (j = 0; j < app.allMessages.length; j++) {
-              // app.allMessage [{objectId: asdjaklsdjlkasjd1239}, {}, {}] <- 'asdjaklsdjlkasjd1239'
-              if (app.allMessages[j].objectId === data.results[i].objectId) {
-                //adding property to results objects to determine if the message was displayed. will default to false
-                data.results[i].displayTest = false;
-                //if key is unique, push new message to allMessage array
-                app.allMessages.unshift(data.results[i]);
-              }
-            }
+        // loop through data and compare data on server to already existing object above
+        for (var i = 0; i < data.results.length; i++) {
+          if (!app.messageStorage[data.results[i].objectId]) {
+            app.messageStorage[data.results[i].objectId] = data.results[i];
+            // roomStorage[roomName] = roomName
+            app.roomStorage[data.results[i].roomname] = data.results[i].roomname;
+            //queue up this message to be displayed
           }
         }
       },
@@ -57,10 +45,45 @@ var app = {
     })
   },
   init: function() {
-    setInterval(app.fetch.bind(app), 3000);
-    setInterval(app.displayAllMessages.bind(app), 3000);
+    // get most recent 100 messages
+    this.fetch('GET', 'order=-createdAt');
+
+    setTimeout(function(){for(var key in app.roomStorage) {
+      var option = $(document.createElement('option'));
+      option.val(_.escape(key));
+      option.text(_.escape(key));
+      $('.room-dropdown').append(option);
+    }}, 2000);
+    // every 3 seconds or sooner we check for the most recent 10 messages
+    // setInterval(app.fetch.bind(app, 'GET', 'order=-createdAt'), 3000);
+    setTimeout(app.displayAllMessages.bind(app), 3000);
   },
-  server: 'https://api.parse.com/1/classes/chatterbox'
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  // queue: {
+  //   storage: {},
+  //   start: 0,
+  //   end: 0,
+
+  //   enqueue: function(value) {
+
+  //     storage[end++] = value;
+  //   },
+
+  //   dequeue: function() {
+
+  //     someInstance.size() && start++;
+  //     var result = storage[start];
+
+  //     delete storage[start];
+
+  //     return result;
+  //   },
+
+  //   size: function() {
+
+  //     return end - start;
+  //   }
+  // }
 };
 
 $(document).ready(function() {
@@ -68,10 +91,32 @@ $(document).ready(function() {
   $('.send-button').on('click', function() {
     var messageObj = {
       username: $('.username-input').val(),
-      text: $('.message-input').val()
+      text: $('.message-input').val(),
+      roomname: $('.room-dropdown').val()
     }
     app.send(messageObj);
   });
+
+  $('.room-dropdown').change(function(){
+    $('.all-messages').empty();
+    //loop through our allMessages object to display messages with roomID which equals value of dropdown
+    if ($(this).val() === 'new-room') {
+      var roomName = prompt('Enter a room name?');
+      var option = $(document.createElement('option'));
+      option.val(_.escape(roomName));
+      option.text(_.escape(roomName));
+      $('.room-dropdown').append(option);
+      $('.room-dropdown').val(roomName);
+    }
+    for(var key in app.messageStorage) {
+      if(app.messageStorage[key].roomname === $(this).val()) {
+        var element = $(document.createElement('div'));
+        element.text(_.escape(app.messageStorage[key].username) + ': ' + _.escape(app.messageStorage[key].text));
+        $('.all-messages').append(element);
+      }
+    }
+  })
+
 });
 // $('message-input').val
 // $('username-input').val
@@ -79,6 +124,10 @@ $(document).ready(function() {
 
 
 app.init();
+
+//create a drowdown in HTML with values of the Room ID in message objects for the dropdown list
+//when a certain room ID is selected, change the displayed messages to filter to that room
+
 
 
 /*app.allMessages.push({
